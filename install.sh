163eb37f645a946b52ba62aac474a121682abd6d
MagnoElMagnífico <marcosgranja317@gmail.com>
2022-08-29 23:43:08 +0200

    Add basic git config

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 
 # Exit on errors
 set -e
@@ -12,7 +12,28 @@ cat << EOF
 (_)__,_|\___/ \__|_| |_|_|\___||___/
 
 By Magno El Magnífico
+
 EOF
 
 # Neovim
 ln -s $PWD/nvim ~/.config
+
+#### git config ####
+read -p "Want to configure git? [y/N]: " -n 1 -r; echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if [[ -x $(command -v git) ]]; then
+        # TODO: Check if already set
+        read -p "Input git username: " git_username
+        read -p "Input git email: " git_email
+    
+        printf "Setting up git profile... "
+    
+        git config --global user.email "$git_email"
+        git config --global user.name "$git_username"
+    
+        printf "Done.\n\n"
+    else
+        echo "Git is not installed or it is not on the path"
+    fi
+fi
