ba92352aa415e688acce99d1e6e51ad17d505610
MagnoElMagnífico <marcosgranja317@gmail.com>
2022-08-31 13:31:02 +0200

    Add variable personalization and tmux install to install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,37 @@ By Magno El Magnífico
 
 EOF
 
+SCRIPT=$(realpath "$0")
+DOTFILES_DIR=$(dirname "$SCRIPT")
+CONFIG_DIR="$HOME/.config"
+
+echo
+echo SCRIPT: $SCRIPT
+echo DOTFILES_DIR: $DOTFILES_DIR
+echo CONFIG_DIR: $CONFIG_DIR
+echo
+
+if [[ -z $XDG_CONFIG_HOME ]]; then
+    printf "XDG_CONFIG_HOME variable not found.\n"
+    read -p "Default is $CONFIG_DIR. Want to change it? [y/N]: " -n 1 -r; echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        while
+	    read -p "Input new config directory: " -r;
+	    [[ -z $REPLY || ! -d $REPLY ]]
+	do printf "Please, input a valid path\n\n"; done
+	CONFIG_DIR=$REPLY
+    fi
+else
+    CONFIG_DIR=$XDG_CONFIG_HOME
+fi
+
 # Neovim
-ln -s $PWD/nvim ~/.config
+ln -s $DOTFILES_DIR/nvim $CONFIG_DIR/nvim
+printf "Neovim config installed successfully\n\n"
+
+# Tmux
+ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
+printf "tmux config installed successfully\n\n"
 
 #### git config ####
 read -p "Want to configure git? [y/N]: " -n 1 -r; echo
@@ -31,8 +60,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     
         git config --global user.email "$git_email"
         git config --global user.name "$git_username"
-	# TODO: Add github password?
-    
+
+        # TODO: Add github password?
         printf "Done.\n\n"
     else
         printf "Git is not installed or it is not on the path.\n\n"
