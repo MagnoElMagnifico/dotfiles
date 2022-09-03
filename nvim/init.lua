32b676ba7ac944ba03163749b49ac1ffff7ac446
MagnoElMagnifico <marcosgranja317@gmail.com>
2022-09-03 21:59:09 +0200

    Refactor nvim config and add telescope mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
 -- TODO: Run terminal command faster
--- Key bindings shortcut --
+
+------------------------------
+---- Util functions ----------
+------------------------------
 -- Modes
 --   normal_mode = 'n',
 --   insert_mode = 'i',
@@ -13,38 +16,95 @@ function map(modes, shortcut, command)
     end
 end
 
-------------------
----- Key Maps ----
-------------------
-
 vim.g.mapleader = ' ' 
 
--- Maybe not that useful
---map('nvx', 'j', 'gj') -- Move down even when the line is wrapped
---map('nvx', 'k', 'gk')
+------------------------------
+---- Plugins -----------------
+------------------------------
+vim.cmd [[ packadd packer.nvim ]]
 
-map('nvx', '=', '$')
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'tpope/vim-surround'
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = {{ 'nvim-lua/plenary.nvim' }}
+    }
+end)
+
+-- Telescope
+map('n', '<Leader>tf', '<CMD>Telescope find_files<Enter>')
+
+map('n', '<Leader>tr', '<CMD>Telescope registers<Enter>')
+map('n', '<Leader>tb', '<CMD>Telescope buffers<Enter>')
+map('n', '<Leader>tm', '<CMD>Telescope marks<Enter>')
+
+map('n', '<Leader>th', '<CMD>Telescope manpages<Enter>')
+map('n', '<Leader>gs', '<CMD>Telescope git_status<Enter>')
+
+-- Netrw
+vim.cmd [[ let g:netrw_liststyle = 1 ]] -- LONG: Extra details
+
+------------------------------
+---- Key Maps ----------------
+------------------------------
+
+---- ??? ----
 --map('nvx', '0', '^') -- ???
+map('nvx', '=', '$') -- ???
 
-map('nvx', '<C-k>', '<C-y>') -- Scroll
-map('nvx', '<C-j>', '<C-e>')
+---- Commands ----
+map('n', '<Leader>ee', '<CMD>Ex<Enter>') -- Show files -- TODO: Bind others: Tex Lex Sex Bex etc
+map('n', '<Leader>eh', '<CMD>Lex 20<Enter>')
+
+-- Obsolete by Telescope
+map('n', '<Leader>r', '<CMD>reg<Enter>') -- Show current registers
+map('n', '<Leader>b', '<CMD>buffers<Enter>') -- Show active buffers
+map('n', '<Leader>m', '<CMD>marks<Enter>') -- Show current marks
+
+map('n', '<Leader>d', '<CMD>bdel<Enter>')
+map('n', '<Leader>n', '<CMD>noh<Enter>')
+map('n', '<Leader>w', '<CMD>w<Enter>')
+map('n', '<Leader>q', '<CMD>qa<Enter>') -- Dangerous & confusing with ñq
+
+---- Misc ----
+map('i', '<C-H>', '<C-w>')
+map('n', 'U',     '<C-R>')  -- Redo with U
+map('n', 'vv',    '<C-v>')
+
+map('v', '<', '<gv') -- Stay in visual mode
+map('v', '>', '>gv')
+
+map('nvx', "'", '`')
 
 -- NOTE: I use a EU keyboard, not US.
 map('nvx', ';', ',') -- Repeat f, F, t, T backwards
 map('nvx', ',', ';') -- Repeat f, F, t, T
 
 -- TODO: Not working
---map('nv', '<A-j>', '<cmd>move .+1<Enter>') -- Move text
---map('nv', '<A-k>', '<cmd>move .-2<Enter>')
---map('x',  '<A-j>', "<cmd>move '>+1<Enter>gv")
---map('x',  '<A-k>', "<cmd>move '<-2<Enter>gv")
+--map('nv', '<A-j>', '<CMD>move .+1<Enter>') -- Move text
+--map('nv', '<A-k>', '<CMD>move .-2<Enter>')
+--map('x',  '<A-j>', "<CMD>move '>+1<Enter>gv")
+--map('x',  '<A-k>', "<CMD>move '<-2<Enter>gv")
 
--- Windows & Buffers --
+map('nvx', '<C-k>', '<C-y>') -- Scroll
+map('nvx', '<C-j>', '<C-e>')
+
+---- Centering cursor ----
+map('nvx', 'n', 'nzz')
+map('nvx', 'N', 'Nzz')
+
+map('nvx', '<C-f>', '<C-f>zz')
+map('nvx', '<C-b>', '<C-b>zz')
+map('nvx', '<C-u>', '<C-u>zz')
+map('nvx', '<C-d>', '<C-d>zz')
+
+---- Windows & Buffers ----
 map('nvx', 'ñ', '<C-w>')
 map('nvx', 'Ñ', '<C-w>')
 
 -- Maybe move between recent windows with <C-l> <C-h> 
-
 map('nvx', '<Right>', '<C-w>l') -- Move between windows
 map('nvx', '<Left>',  '<C-w>h')
 map('nvx', '<Up>',    '<C-w>k')
@@ -55,54 +115,29 @@ map('nvx', '<S-Left>',  '<C-w><')
 map('nvx', '<S-Up>',    '<C-w>+')
 map('nvx', '<S-Down>',  '<C-w>-')
 
-map('n', '<C-Right>', '<cmd>tabn<Enter>') -- Move between tabs
-map('n', '<C-Left>',  '<cmd>tabp<Enter>')
+map('n', '<C-Right>', '<CMD>tabn<Enter>') -- Move between tabs
+map('n', '<C-Left>',  '<CMD>tabp<Enter>')
 
-map('n', '<C-Down>', '<cmd>bnext<Enter>') -- Move between buffers -- TODO: Problems in Netrw
-map('n', '<C-Up>',   '<cmd>bprevious<Enter>')
+map('n', '<C-Down>', '<CMD>bnext<Enter>') -- Move between buffers -- TODO: Problems in Netrw
+map('n', '<C-Up>',   '<CMD>bprevious<Enter>')
 
--- Exiting --
+---- Exiting ----
 map('t', '<Esc>', '<C-\\><C-N>') -- Exit terminal
 map('t', 'jk',    '<C-\\><C-N>')
 map('t', 'kj',    '<C-\\><C-N>')
 map('i', 'jk',    '<Esc>') -- Exit insert mode
 map('i', 'kj',    '<Esc>')
 
--- Others --
-map('n', '<Leader>ee', '<cmd>Ex<Enter>') -- Show files -- TODO: Bind others: Tex Lex Sex Bex etc
-map('n', '<Leader>eh', '<cmd>Lex 20<Enter>')
-
-map('n', '<Leader>r', '<cmd>reg<Enter>') -- Show current registers
-map('n', '<Leader>b', '<cmd>buffers<Enter>') -- Show active buffers
-map('n', '<Leader>m', '<cmd>marks<Enter>') -- Show current marks
-
-map('n', '<Leader>n', '<cmd>noh<Enter>')
-map('n', '<Leader>d', '<cmd>redraw<Enter>')
-map('n', '<Leader>w', '<cmd>w<Enter>')
-map('n', '<Leader>q', '<cmd>qa<Enter>') -- Dangerous & confusing with ñq
-
-map('nvx', 'n', 'nzz')
-map('nvx', 'N', 'Nzz')
-
-map('nvx', "'", '`')
-
-map('i', '<C-H>', '<C-w>')
-map('n', 'U',     '<C-R>')  -- Redo with U
-map('n', 'vv',    '<C-v>') -- In my terminal CTRL-V pastes
-
-map('v', '<', '<gv') -- Stay in visual mode
-map('v', '>', '>gv')
-
-----------------------
----- Autocommands ----
-----------------------
+------------------------------
+---- Autocommands ------------
+------------------------------
 
 -- Remove trailing whitespace
 vim.cmd [[ autocmd BufWritePre * :%s/\[\s\n\]\+$//e ]]
 
------------------
----- Options ----
------------------
+------------------------------
+---- Options -----------------
+------------------------------
 vim.o.fileencoding = 'utf-8'
 vim.o.langmenu = 'en_US.UTF8' -- Language
 
@@ -111,7 +146,7 @@ vim.opt.path:append('**')
 --vim.o.spell = true
 vim.o.spelllang = 'es,en'
 
-vim.o.autoindent = true    
+vim.o.autoindent = true
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 2
@@ -136,20 +171,3 @@ vim.o.smartcase = true  -- Search case insensitive, unless the search term conta
 
 vim.o.splitbelow = true
 vim.o.splitright = true
-
---------------------
--- Plugins ---------
---------------------
-vim.cmd [[ packadd packer.nvim ]]
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'tpope/vim-surround'
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-end)
-
--- Netrw
-vim.cmd [[ let g:netrw_liststyle = 1 ]] -- LONG: Extra details
