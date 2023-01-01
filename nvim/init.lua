bc819e09433062b54652ea231cb57bc6b43e9420
MagnoElMagnifico <marcosgranja317@gmail.com>
2023-01-01 20:33:22 +0100

    Upgrade NVim config thanks to ThePrimeagen

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,182 +1,67 @@
--- TODO: Run terminal command faster
--- TODO: Debugger
--- TODO: LSP: Rust, C/C++, Python
-
-------------------------------
----- Util functions ----------
-------------------------------
--- Modes
---   normal_mode = 'n',
---   insert_mode = 'i',
---   visual_mode = 'v',
---   visual_block_mode = 'x', -- ???
---   term_mode = 't',
---   command_mode = 'c',
-function map(modes, shortcut, command)
-    for mode in modes:gmatch"." do
-        vim.api.nvim_set_keymap(mode, shortcut, command, {noremap = true, silent = true})
-    end
-end
-
-vim.g.mapleader = ' ' 
-
-------------------------------
----- Plugins -----------------
-------------------------------
+---- Install plugins ----
 vim.cmd [[ packadd packer.nvim ]]
 
+-- Remember to :PackerSync after changing something
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'tpope/vim-surround'
+
+    ---- Telescope ----
     use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    -- TODO?: theprimeagen/harpoon
+
+    ---- LSP ----
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- :Mason to manage LSP servers
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    ---- Tree sitter ----
+    use (
+        'nvim-treesitter/nvim-treesitter',
+        { run = ':TSUpdate' }
+    )
+    -- Show Abstract Syntax Tree with :TSPlaygroundToggle
+    -- use 'nvim-treesitter/playground'
+
+    ---- Others ----
+    use 'tpope/vim-surround'
+    use 'tpope/vim-commentary'
+    -- use 'tpope/vim-fugitive' -- Git
+    -- use 'mbbill/undotree'
+
+    -- Colorschemes: https://vimcolorschemes.com/
+    use 'navarasu/onedark.nvim'
 end)
 
--- Telescope
-require('telescope').setup {
-    defaults = {
-        file_ignore_patterns = {"build", "target", "nbproject"}
-    }
-}
+---- More configuration ----
+require('remap')
+require('options')
 
-map('n', '<Leader>f', '<CMD>Telescope find_files<Enter>')
-map('n', '<Leader>g', '<CMD>Telescope live_grep<Enter>')
+-- Preferred colorscheme
+local onedark = require('onedark')
+-- Options: dark, darker, cool, deep, warm, warmer, light
+onedark.setup { style = 'warmer' } 
+onedark.load()
 
-map('n', '<Leader>tr', '<CMD>Telescope registers<Enter>')
-map('n', '<Leader>tb', '<CMD>Telescope buffers<Enter>')
-map('n', '<Leader>tm', '<CMD>Telescope marks<Enter>')
-
-map('n', '<Leader>th', '<CMD>Telescope manpages<Enter>')
-map('n', '<Leader>gs', '<CMD>Telescope git_status<Enter>')
-
--- Netrw
---vim.cmd [[ let g:netrw_liststyle = 1 ]] -- LONG: Extra details
-
-------------------------------
----- Key Maps ----------------
-------------------------------
-
----- ??? ----
---map('nvx', '0', '^') -- ???
-map('nvx', '=', '$') -- ???
-
----- Commands ----
-map('n', '<Leader>ee', '<CMD>Ex<Enter>') -- Show files -- TODO: Bind others: Tex Lex Sex Bex etc
-map('n', '<Leader>eh', '<CMD>Lex 20<Enter>')
-
--- Obsolete by Telescope
-map('n', '<Leader>r', '<CMD>reg<Enter>') -- Show current registers
-map('n', '<Leader>b', '<CMD>buffers<Enter>') -- Show active buffers
-map('n', '<Leader>m', '<CMD>marks<Enter>') -- Show current marks
-
-map('n', '<Leader>d', '<CMD>bdel<Enter>')
-map('n', '<Leader>n', '<CMD>noh<Enter>')
-map('n', '<Leader>w', '<CMD>w<Enter>')
-map('n', '<Leader>q', '<CMD>qa<Enter>') -- Dangerous & confusing with ñq
-
----- Misc ----
-map('i', '<C-H>', '<C-w>')
-map('n', 'U',     '<C-R>')  -- Redo with U
-map('n', 'vv',    '<C-v>')
-
-map('v', '<', '<gv') -- Stay in visual mode
-map('v', '>', '>gv')
-
-map('nvx', "'", '`')
-
--- NOTE: I use a EU keyboard, not US.
-map('nvx', ';', ',') -- Repeat f, F, t, T backwards
-map('nvx', ',', ';') -- Repeat f, F, t, T
-
--- TODO: Not working
---map('nv', '<A-j>', '<CMD>move .+1<Enter>') -- Move text
---map('nv', '<A-k>', '<CMD>move .-2<Enter>')
---map('x',  '<A-j>', "<CMD>move '>+1<Enter>gv")
---map('x',  '<A-k>', "<CMD>move '<-2<Enter>gv")
-
-map('nvx', '<C-k>', '<C-y>') -- Scroll
-map('nvx', '<C-j>', '<C-e>')
-
----- Centering cursor ----
-map('nvx', 'n', 'nzz')
-map('nvx', 'N', 'Nzz')
-
-map('nvx', '<C-f>', '<C-f>zz')
-map('nvx', '<C-b>', '<C-b>zz')
-map('nvx', '<C-u>', '<C-u>zz')
-map('nvx', '<C-d>', '<C-d>zz')
-
----- Windows & Buffers ----
-map('nvx', 'ñ', '<C-w>')
-map('nvx', 'Ñ', '<C-w>')
-
--- Maybe move between recent windows with <C-l> <C-h> 
-map('nvx', '<Right>', '<C-w>l') -- Move between windows
-map('nvx', '<Left>',  '<C-w>h')
-map('nvx', '<Up>',    '<C-w>k')
-map('nvx', '<Down>',  '<C-w>j')
-
-map('nvx', '<S-Right>', '<C-w>>') -- Resize windows
-map('nvx', '<S-Left>',  '<C-w><')
-map('nvx', '<S-Up>',    '<C-w>+')
-map('nvx', '<S-Down>',  '<C-w>-')
-
-map('n', '<C-Right>', '<CMD>tabn<Enter>') -- Move between tabs
-map('n', '<C-Left>',  '<CMD>tabp<Enter>')
-
-map('n', '<C-Down>', '<CMD>bnext<Enter>') -- Move between buffers -- TODO: Problems in Netrw
-map('n', '<C-Up>',   '<CMD>bprevious<Enter>')
-
----- Exiting ----
-map('t', '<Esc>', '<C-\\><C-N>') -- Exit terminal
-map('t', 'jk',    '<C-\\><C-N>')
-map('t', 'kj',    '<C-\\><C-N>')
-map('i', 'jk',    '<Esc>') -- Exit insert mode
-map('i', 'kj',    '<Esc>')
-
-------------------------------
----- Autocommands ------------
-------------------------------
-
--- Remove trailing whitespace
-vim.cmd [[ autocmd BufWritePre * :%s/\[\s\n\]\+$//e ]]
-
-------------------------------
----- Options -----------------
-------------------------------
-vim.o.fileencoding = 'utf-8'
-vim.o.langmenu = 'en_US.UTF8' -- Language
-
-vim.opt.path:append('**')
-
---vim.o.spell = true
-vim.o.spelllang = 'es,en'
-
-vim.o.autoindent = true
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-
-vim.o.mouse = 'a'
-
-vim.o.wrap = true
-vim.o.showbreak = '\\' -- Wrapped text
-
-vim.o.scrolloff = 4
-vim.o.sidescrolloff = 4
-
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.colorcolumn = 80
-
-vim.o.timeoutlen = 500
-vim.o.cmdheight = 1
-
-vim.o.smartcase = true  -- Search case insensitive, unless the search term contains an upper case
-
-vim.o.splitbelow = true
-vim.o.splitright = true
---
+++ nvim/lua/remap.lua
@@ -0,0 +1,73 @@
+-- Modes
+--   n    Normal
+--   i    Insert
+--   c    Command
+--   v    Visual and Select
+--   x    Visual
+--   s    Select
+--   t    Terminal (insert mode inside of a terminal)
+local map = vim.keymap.set
+vim.g.mapleader = ' '
+
+---- Misc ----
+map({'n', 'v'}, '+', '$')
+map({'n', 'v'}, '-', '^')
+
+map('i', '<C-H>', '<C-w>')    -- Delete word in Insert mode
+map('n', 'U',     '<C-R>')    -- Redo with U
+
+-- Stay in visual mode after identing
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
+map({'n', 'x'}, "'", '`')
+
+-- NOTE: I use a EU keyboard, not US.
+map({'n', 'v'}, ';', ',')     -- Repeat f, F, t, T backwards
+map({'n', 'v'}, ',', ';')     -- Repeat f, F, t, T
+
+-- Yank and Paste
+map('v', '<Leader>p', '"_dP')
+map('n', '<Leader>Y', '"+Y')
+map({'n', 'v'}, '<Leader>y', '"+y')   -- Yank to system clipboard
+map({'n', 'v'}, '<Leader>d', '"_d')
+
+---- Commands ----
+-- Refactor symbol (Change)
+map('n', '<Leader>c', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
+map('n', '<Leader>x', vim.cmd.bdel)
+map('n', '<Leader>w', vim.cmd.write)
+
+-- TODO: Not working
+-- Move text
+--map('n', '<A-j>', function() vim.cmd.move('+1') end)
+--map('n', '<A-k>', function() vim.cmd.move('-2') end)
+--map('v', 'J', "<CMD>move '>+1<Enter>gv=gv")
+--map('v', 'K', "<CMD>move '<-2<Enter>gv=gv")
+
+-- Scroll
+map({'n', 'v'}, '<C-k>', '<C-y>') -- Scroll
+map({'n', 'v'}, '<C-j>', '<C-e>')
+
+---- Centering cursor ----
+map({'n', 'v'}, 'n', 'nzz')
+map({'n', 'v'}, 'N', 'Nzz')
+
+map({'n', 'v'}, '<C-b>', '<C-b>zz')
+map({'n', 'v'}, '<C-f>', '<C-f>zz')
+map({'n', 'v'}, '<C-d>', '<C-d>zz')
+map({'n', 'v'}, '<C-u>', '<C-u>zz')
+
+---- Windows & Buffers & Tabs ----
+map({'n', 'v'}, 'ñ', '<C-w>')
+map({'n', 'v'}, 'Ñ', '<C-w>')
+
+map('n', '<C-Right>', vim.cmd.tabn) -- Move between tabs
+map('n', '<C-Left>',  vim.cmd.tabp)
+
+---- Exiting ----
+map('t', '<Esc>', '<C-\\><C-N>')    -- Exit terminal
+map('t', 'jk',    '<C-\\><C-N>')
+map('t', 'kj',    '<C-\\><C-N>')
+map('i', 'jk',    '<Esc>')          -- Exit insert mode
+map('i', 'kj',    '<Esc>')
