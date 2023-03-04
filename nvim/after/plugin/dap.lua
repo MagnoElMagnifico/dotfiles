0010f9b837c6aab4c1405ef2070cc32007bb4efb
MarcosG <marcosgranja317@gmail.com>
2023-03-04 19:07:51 +0100

    Using Termdebug now

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,2 +1,17 @@
 -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 -- https://github.com/nvim-lua/wishlist/issues/37
+
+-- Maybe even better than DAP:
+vim.cmd.packadd('termdebug')
+vim.g.termdebug_wide = 80
+
+local map = vim.keymap.set
+
+map('n', '<Leader>db', vim.cmd.Break)  -- Add breakpoint
+map('n', '<Leader>dB', vim.cmd.Clear)  -- Remove breakpoint
+map('n', '<Leader>ds', vim.cmd.Step)
+map('n', '<Leader>do', vim.cmd.Over)
+-- map('n', '<Leader>du', vim.cmd.Until)
+-- map('n', '<Leader>df', vim.cmd.Finish)
+-- map('n', '<Leader>dc', vim.cmd.Continue)
+-- map('n', '<Leader>dp', vim.cmd.Stop)
