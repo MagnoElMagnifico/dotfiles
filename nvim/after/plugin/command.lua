146772a6c238294a447ee8f37348caeae56c7116
MarcosG <marcosgranja317@gmail.com>
2023-03-04 19:07:10 +0100

    Add vertical and horizontal splits

--- nvim/after/plugin/command.lua
+++ nvim/after/plugin/command.lua
@@ -2,7 +2,8 @@
 local map = vim.keymap.set
 
 vim.api.nvim_set_var('magno_cmd', '')
-map('n', '<Leader>c', function()
+
+map('n', '<Leader>cv', function()
   vim.ui.input({
     prompt = 'Command > ',
     default = nil,
@@ -16,14 +17,30 @@ map('n', '<Leader>c', function()
     end)
 end, { desc = 'Run command' })
 
+map('n', '<Leader>ch', function()
+  vim.ui.input({
+    prompt = 'Command > ',
+    default = nil,
+    completion = 'file' -- TODO: complete as bash
+  },
+    function(input)
+      if (not (input == nil or input == '')) then
+        vim.g.magno_cmd = input
+        vim.fn.execute(':split term://' .. vim.g.magno_cmd)
+      end
+    end)
+end, { desc = 'Run command' })
+
 -- Repeat last command
-map('n', '<Leader>C', function()
+map('n', '<Leader>CV', function()
   if (not (vim.g.magno_cmd == nil or vim.g.magno_cmd == '')) then
     vim.fn.execute(':vs term://' .. vim.g.magno_cmd)
   end
 end, { desc = 'Repeat last command' })
 
--- Exit terminal
-vim.api.nvim_create_autocmd({ 'TermOpen' }, {
-  callback = function() map('n', 'q', 'i<C-D>', { buffer = true }) end
-})
+map('n', '<Leader>CH', function()
+  if (not (vim.g.magno_cmd == nil or vim.g.magno_cmd == '')) then
+    vim.fn.execute(':split term://' .. vim.g.magno_cmd)
+  end
+end, { desc = 'Repeat last command' })
+
