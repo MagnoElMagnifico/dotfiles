da5567263a14cfa6ec574af803121d14e8675dee
MarcosG <marcosgranja317@gmail.com>
2023-04-12 22:48:15 +0200

    Neovide config

---
+++ nvim/after/plugin/neovide.lua
@@ -0,0 +1,8 @@
+-- Configuracion for Neovide frontend
+vim.o.guifont = "Fira Code:h7"
+vim.g.neovide_scroll_animation_length = 0.3
+vim.g.neovide_cursor_animation_length = 0.13
+vim.g.neovide_cursor_trail_size = 0.8
+-- railgun, torpedo, pixiedust, sonicboom, ripple, wireframe
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_cursor_vfx_particle_density = 7.0
