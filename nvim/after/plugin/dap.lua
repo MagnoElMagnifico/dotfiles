86e1628960556539f0124ee143da9f3f2749101d
MarcosG <marcosgranja317@gmail.com>
2023-07-12 13:55:56 +0200

    Add TODO link and format

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,5 +1,6 @@
 -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 -- https://github.com/nvim-lua/wishlist/issues/37
+-- https://github.com/puremourning/vimspector
 
 -- Maybe even better than DAP:
 vim.cmd.packadd('termdebug')
@@ -7,10 +8,11 @@ vim.g.termdebug_wide = 80
 
 local map = vim.keymap.set
 
-map('n', '<Leader>db', vim.cmd.Break)  -- Add breakpoint
-map('n', '<Leader>dB', vim.cmd.Clear)  -- Remove breakpoint
-map('n', '<Leader>n',  vim.cmd.Step)   -- Advance to next line
-map('n', '<Leader>o',  vim.cmd.Over)   -- Advance to the next line without entering functions
-map('n', '<Leader>du', vim.cmd.Until)  -- Run until cursor
-map('n', '<Leader>dc', vim.cmd.Continue)  -- Continue until next breakpoint
-map('n', '<Leader>df', vim.cmd.Finish)    -- Continue until the end of the function
+map('n', '<Leader>db', vim.cmd.Break)    -- Add breakpoint
+map('n', '<Leader>dB', vim.cmd.Clear)    -- Remove breakpoint
+map('n', '<Leader>n',  vim.cmd.Step)     -- Advance to next line
+map('n', '<Leader>o',  vim.cmd.Over)     -- Advance to the next line without entering functions
+map('n', '<Leader>du', vim.cmd.Until)    -- Run until cursor
+map('n', '<Leader>dc', vim.cmd.Continue) -- Continue until next breakpoint
+map('n', '<Leader>df', vim.cmd.Finish)   -- Continue until the end of the function
+
