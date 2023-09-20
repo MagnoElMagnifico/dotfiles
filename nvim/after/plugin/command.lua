46d91d5491afb9885cad7fbb3bcf8aa55f8aa1fc
MarcosG <marcosgranja317@gmail.com>
2023-09-20 09:08:19 +0200

    Fix and simplify command.lua

--- nvim/after/plugin/command.lua
+++ nvim/after/plugin/command.lua
@@ -1,23 +1,22 @@
 -- Asks for a command and runs it in a vertical split window
 vim.api.nvim_set_var('magno_cmd', '')
 
-local function term_cmd(split, cmd, opts)
+local function term_cmd(split, opts)
     if (#opts.fargs ~= 0) then
-      vim.g.magno_cmd = table.concat(opts.fargs, ' ')
+      vim.api.nvim_set_var('magno_cmd', table.concat(opts.fargs, ' '))
     end
 
     if (opts.bang) then
       vim.fn.execute(':' .. split .. ' | term ')
     else
-      vim.fn.execute(':' .. split .. ' | term ' .. cmd)
+      vim.fn.execute(':' .. split .. ' | term ' .. vim.g.magno_cmd)
     end
 end
 
-local attributes = { nargs = '*', complete = 'shellcmd', bang = true }
-vim.api.nvim_create_user_command('Vter', function(opts) term_cmd('vs', vim.g.magno_cmd, opts) end, attributes)
-vim.api.nvim_create_user_command('Hter', function(opts) term_cmd('sp', vim.g.magno_cmd, opts) end, attributes)
+vim.api.nvim_create_user_command('Vterm', function(opts) term_cmd('vs', opts) end, { nargs = '*', bang = true })
+vim.api.nvim_create_user_command('Hterm', function(opts) term_cmd('sp', opts) end, { nargs = '*', bang = true })
 
 -- Quickly open a Python interactive console
-vim.api.nvim_create_user_command('Vpy', function(opts) term_cmd('vs', 'python3', opts) end, { nargs = 0, bang = false })
-vim.api.nvim_create_user_command('Hpy', function(opts) term_cmd('sp', 'python3', opts) end, { nargs = 0, bang = false })
+vim.api.nvim_create_user_command('Vpy', ':vs | term python3', { nargs = 0, bang = false })
+vim.api.nvim_create_user_command('Hpy', ':sp | term python3', { nargs = 0, bang = false })
 
