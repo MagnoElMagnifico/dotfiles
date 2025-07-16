fb741cf539c12af7fcac5dc282cbd6924193f63c
MarcosG <marcosgranja317@gmail.com>
2025-07-16 18:46:33 +0200

    Revise treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,34 @@
 return {
+  -- Tree-sitter is a parsing library that generates a tree of some source code.
+  -- As a result, Neovim has more information about the code since it
+  -- understands how it is structured, so it can provide additional
+  -- functionalities. These are provided by this package as different modules:
+  -- highlight, incremental_selection, indent (see details below).
+  --
+  -- There are additional nvim-treesitter modules that you can use to interact
+  -- with nvim-treesitter, but as separate plugins:
+  --
+  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+  --    - Treesitter + textobjects:  https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  --
+  -- The plugin also includes some functions:
+  --
+  --    - :h nvim-treesitter-utils
+  --    - :h nvim-treesitter-functions
+  --
+  -- COMMANDS
+  --    :TSInstall {lang}   Install a new parser
+  --    :TSInstallInfo      List of supported languages
+  --    :TSUpdate [lang]    Update a parser or all the installed ones
+  --    :TSModuleInfo       Show the state of every module (highlight,
+  --                        incremental_selection, indent) for every parser
+  --    :TSToggle {mod}     Dis/enable modules for the current session
+  --    :TSBufToggle {mod}  Dis/enable modules for the current buffer
+  --
   {
     'nvim-treesitter/nvim-treesitter',
-    build = '<cmd>TSUpdate',
+    lazy = false,        -- Lazy loading is not supported
+    build = ':TSUpdate', -- Must update parsers at the same time
 
     opts = {
       ensure_installed = {
@@ -23,32 +50,42 @@ return {
       -- Autoinstall languages that are not installed
       auto_install = true,
 
+      ---- MODULES ------------------------------------------------------------
+      -- Consistent syntax highlighting functionalities, even with modifications
+      -- or errors (as you're typing).
       highlight = {
         enable = true,
-        -- -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        -- --  If you are experiencing weird indenting issues, add the language to
-        -- --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        -- additional_vim_regex_highlighting = { 'python' },
+
+        -- Disable in files with more than 5K lines, which could slow down Neovim
+        disable = function(_, bufnr)
+          return vim.api.nvim_buf_line_count(bufnr) > 5000
+        end,
       },
 
-      indent = { enable = true, disable = { 'python' } },
+      -- Indentation based on tree-sitter for the '=' operator
+      indent = {
+        enable = true,
+      },
+
+      -- Incremental selection based on the named nodes from the grammar.
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection    = 'gnn',  -- Start incremental selection
+          scope_incremental = 'grc',  -- Increment to upper scope
+          node_incremental  = 'grn',  -- Increment selection to named parent
+          node_decremental  = 'grm',  -- Decrement to previous named node
+        },
+      },
     },
 
     config = function(_, opts)
-      -- See `:help nvim-treesitter`
-      -- Prefer git instead of curl in order to improve connectivity in some environments
+      -- Prefer git instead of curl in order to improve connectivity in some
+      -- environments
       require('nvim-treesitter.install').prefer_git = true
 
-      ---@diagnostic disable-next-line: missing-fields
+      -- Now apply the configuration
       require('nvim-treesitter.configs').setup(opts)
-
-      -- TODO:
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   }, -- treesitter
 } -- return
