015e65c24ab0b5d2bbefc26e9d93bcf013202c23
MarcosG <marcosgranja317@gmail.com>
2025-07-31 13:46:23 +0200

    Don't show fastfetch on neovim, tmux or ssh

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,15 @@
 function fish_greeting
-    if type -q fastfetch
-        command fastfetch
+    # Run fastfetch on startup if:
+    #   - the command exists
+    #   - not running inside Vim or Neovim
+    #   - not running inside tmux or zellij
+    #   - not running inside SSH
+    if type -q fastfetch; \
+        and not set -q NVIM; \
+        and not set -q VIMRUNTIME; \
+        and not set -q TMUX; \
+        and not set -q ZELLIJ; \
+        and not set -q SSH_TTY
+            command fastfetch
     end
 end
