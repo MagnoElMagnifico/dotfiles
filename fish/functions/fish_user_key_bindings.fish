9d324082c486481385e7e42e16c01fbe2bb4143b
MarcosG <marcosgranja317@gmail.com>
2025-09-22 12:52:57 +0200

    fish bindings

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,17 +1,45 @@
 function fish_user_key_bindings
-    fish_default_key_bindings
+    # I will use arrow for previous commands
+    bind ctrl-p prevd-or-backward-word # prevd when empty
+    bind ctrl-n nextd-or-forward-word  # nextd when empty
 
-    # TODO: better bindings (some of them are picked by the terminal)
-    # bind ctrl-f accept-autosuggestion
-    # bind ctrl-s edit_command_buffer
-    # bind ctrl-t transpose-words
-    # bind ctrl-m __fish_list_current_token # TODO: does not work
+    # Completion
+    bind ctrl-f accept-autosuggestion
+    bind ctrl-r history-pager        # Search command history
+    bind ctrl-s pager-toggle-search  # Search pager
+
+    # Common CLI editing
+    bind ctrl-b edit_command_buffer
+    bind ctrl-l clear-screen
+    bind ctrl-c clear-commandline
+    bind ctrl-d delete-or-exit
+    bind ctrl-z undo
+
+    # Emacs things
+    bind ctrl-e end-of-line
+    bind ctrl-a beginning-of-line
+    bind ctrl-h backward-kill-word           # ctrl-backspace
+    bind ctrl-w backward-kill-path-component # delete previous word
+    bind ctrl-u backward-kill-line           # delete to start
+    bind ctrl-k kill-line                    # delete until end
+    bind ctrl-t transpose-words              # swap two previous words
+
+    bind ctrl-y yank
+    bind ctrl-v fish_clipboard_paste
+    bind ctrl-x fish_clipboard_copy
+
+    # Check with `fish_key_reader` because terminals things
+    # - ctrl-m == enter
+    # - ctrl-i == tab
+    # - ctrl-h == ctrl-backspace
     #
-    # # As documentation
-    # bind ctrl-w backward-kill-path-component # delete previous word
-    # bind ctrl-u backward-kill-line           # delete to start
-    # bind ctrl-k kill-line                    # delete until end
+    # Empty keys
+    # - ctrl-q
+    # - ctrl-o
+    # - ctrl-g
+    # - ctrl-j
 
+    # # Vim + Emacs
     # # Execute this once per mode that emacs bindings should be used in
     # fish_default_key_bindings -M insert
     #
@@ -20,7 +48,4 @@ function fish_user_key_bindings
     # # resetting all bindings.
     # # The argument specifies the initial mode (insert, "default" or visual).
     # fish_vi_key_bindings --no-erase insert
-    #
-    # #### Custom bindings ####
-    # bind -M insert ctrl-g accept-autosuggestion
 end
